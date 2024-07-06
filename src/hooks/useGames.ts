7e189b687e3308e175351c434760ffaf53d55654
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-07-06 21:12:49 +0545

    loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,23 +24,28 @@ import { CanceledError } from "axios";
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]); // to store game object
     const [error, setError] = useState(""); // for error message
+    const [isLoading, setLoading] = useState(false);
   
     //to send fetch req to backend
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true)
       apiClient
         .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {setGames(res.data.results);
+            setLoading(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
             setError(err.message)
+            setLoading(false);
         });
 
         return () => controller.abort();
     }, []);
 
-    return {games, error}
+    return {games, error, isLoading}
 
 }
 
